4ad384b7fc477f8a572664752e55b3b5c4a75898
farhandotme <princeofficial355@gmail.com>
2024-12-07 00:19:34 +0530

    handelCheckbox

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,28 @@ import { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { v4 as uuidv4 } from "uuid";
+uuidv4();
 
 function App() {
-  const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handelAdd = () => {
+    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
+    setTodo("");
+  };
+
+  const handelCheckBox = (e) => {
+    let id = e.target.name;
+    let index = todos.findIndex((item) => {
+      return (item.id = id);
+    });
+
+    let newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  };
 
   return (
     <>
@@ -16,29 +34,50 @@ function App() {
             className="text-3xl border-2 border-emerald-900 w-1/3 rounded-md px-3 py-1"
             type="text"
             placeholder="Enter your todo"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
           />
           <button
-            onClick={() => setTodos([...todos, todo])}
+            onClick={handelAdd}
             className="text-3xl ml-5 text-white bg-emerald-800 border-2 border-emerald-900 hover:border-emerald-900 hover:bg-emerald-100 hover:text-emerald-900 transition-colors rounded-md px-3 py-2"
           >
             <FaPlus />
           </button>
         </div>
-        <div className="flex justify-center items-center mt-10">
-          <div>
-            <span className="text-2xl text-emerald-900 mt">
-              dolor sit, amet consectetur adipisicing.
-            </span>
-          </div>
-          <div className="flex ml-10">
-            <button className="mr-4 bg-gray-800 px-4 py-2 text-white rounded">
-              Edit
-            </button>
-            <button className="mr-8 bg-red-700 px-4 py-2 text-white rounded">
-              Delete
-            </button>
-          </div>
-        </div>
+        {todos.map((item) => {
+          return (
+            <div className="flex justify-center">
+              <div className="flex p-5 rounded-lg justify-between items-center w-1/3 mt-10 border-2 bg-emerald-200">
+                <input
+                  onChange={handelCheckBox}
+                  type="checkbox"
+                  value={item.isCompleted}
+                  className=""
+                  name={item.id}
+                />
+                <div>
+                  <span
+                    className={
+                      item.isCompleted
+                        ? "line-through text-2xl text-emerald-900 mt"
+                        : "text-2xl text-emerald-900 mt"
+                    }
+                  >
+                    {item.todo}
+                  </span>
+                </div>
+                <div className="flex ml-10">
+                  <button className="mr-4 bg-gray-800 px-4 py-2 text-white rounded">
+                    Edit
+                  </button>
+                  <button className=" bg-red-700 px-4 py-2 text-white rounded">
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
